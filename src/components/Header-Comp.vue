8a539bb166943f7f561e6c53d00a649652f95421
Amoebajelek <dinoimpostor00@gmail.com>
2022-04-16 00:39:57 +0700

    last : custom events

---
+++ src/components/Header-Comp.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>VueJs dan {{ text }} </h3>
+  <button @click="changeText">Ubah Text</button>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+  props: ["text"],
+  data(){
+    return{
+      newText: "ini adalah text dari header.vue"
+    }
+  },
+  methods: {
+    changeText(){
+      this.$emit("changeTitle", this.newText)
+    }
+  },
+  emits: ["changeTitle"]
+};
+</script>
+
+<style></style>
